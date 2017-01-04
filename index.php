dee247714dbdb47a6f4fa330c9f2c46de61e365d
Corentin MOREAU <corentin.moreau2@gmail.com>
2017-01-04 15:18:53 +0100

    maj interfaces

--- index.php
+++ index.php
@@ -1,60 +1,80 @@
 <html>
 <head>
   <title>Toilette connecté</title>
-  <meta http-equiv="Refresh" content="4">
+  <meta http-equiv="refresh" content="0; url=interface/" />
   <!-- Compiled and minified CSS -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/css/materialize.min.css">
 
   <!-- Compiled and minified JavaScript -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
-
+  <script type="text/javascript">
+              window.location.href = "interface/"
+          </script>
   <script src="pace.min.js"></script>
   <link rel="stylesheet" href="style-pace.css">
 
 </head>
 <body>
   <?php
-
-  $section = file_get_contents('/dev/cu.usbmodem1411', NULL, NULL, NULL, 2);
-  var_dump($section);
-
+/*
+  //$section = file_get_contents('/dev/cu.usbmodem1411', NULL, NULL, NULL, 2);
+  //var_dump($section);
+*/
   ?>
   <br>
   <?php
   // Turn Led ON ?>
+
+
+  <div class="section no-pad-bot" id="index-banner">
+          <div class="container">
+            <h1 class="header center-on-small-only">Materialize</h1>
+            <div class="row center">
+              <h4 class="header col s12 light center">Un framework front-end adaptatif et moderne basé sur le Material Design</h4>
+            </div>
+            <div class="row center">
+              <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light">Démarrer</a>
+            </div>
+            <div class="row center"><a class="red-text text-lighten-4" href="https://github.com/Dogfalo/materialize">beta release v0.97.8</a></div>
+
+            <div class="buysellads-header buysellads-homepage row center">
+              <!-- CarbonAds Zone Code -->
+              <script async="" type="text/javascript" src="//cdn.carbonads.com/carbon.js?zoneid=1673&amp;serve=C6AILKT&amp;placement=materializecss" id="_carbonads_js"></script>
+            </div>
+            <br>
+
+          </div>
+          <div class="github-commit">
+            <div class="container">
+              <div class="commit">Latest Commit on Github: &nbsp;&nbsp; <a href="https://github.com/Dogfalo/materialize/commit/817fd13f40193742ad3648a137b4b43d0976e324" class="sha">817fd13f40193742ad3648a137b4b43d0976e324</a> &nbsp;&nbsp; <span class="date">about 9 hours ago</span> <a id="github-button" href="https://github.com/Dogfalo/materialize" class="btn-flat right grey-text text-lighten-5 waves-effect waves-light hide-on-small-only">Github</a> </div>
+            </div>
+          </div>
+        </div>
+
+
+
+
   <div class="container center-align">
     <h1>Interface</h1>
-    <div class="row">
-        <form action="index.php" method="POST">
-          <input type="hidden" name="turn" value="libre" />
-          <input class="btn waves-effect waves-light btn-large green darken-1" type="Submit" value="Libre">
-        </form>
-        <?php
-        // Turn Led OFF ?>
-        <form action="index.php" method="POST">
-          <input type="hidden" name="turn" value="occupe" />
-          <input class="btn waves-effect waves-light btn-large deep-orange darken-2" type="Submit" value="Occupé">
-        </form>
-      </p>
       </div>
     </div>
   <?php
-  echo $contents;
+  /*echo $contents;
   //if ($_POST['turn']=="libre"){
   if(strpos($section,'1') !== false) {
-    echo '<h4 class="center-align">Le toilette est occupé</h4>';
+    echo '<h1 class="center-align">Le toilette est occupé</h1>';
     //fwrite($port, "ETEINT");
     fwrite($file, "ETEINT");
   }
   //if ($_POST['turn']=="occupe")
   else{
-    echo '<h4 class="center-align">Le toilette est libre</h4>';
+    echo '<h1 class="center-align">Le toilette est libre</h1>';
     //fwrite($port, "ALLUME");
     fwrite($file, "ALLUME");
 
   }
-  var_dump($contents);
+  //var_dump($contents);
   fclose($port);
-  fclose($file);
+  fclose($file);*/
   ?>
 </body></html>
---
+++ interface/index.php
@@ -0,0 +1,102 @@
+<?php
+$desc_fichier_arduino = '/dev/cu.usbmodem1411';
+
+if(file_exists($desc_fichier_arduino))
+  $etat = file_get_contents($desc_fichier_arduino, NULL, NULL, NULL, 2);
+else
+  $etat = '2';
+
+function changerCouleur(){
+  global $etat;
+  if(strpos($etat,'1') !== false){
+    echo "orange";
+  }else if(strpos($etat,'0') !== false){
+    echo "green";
+  } else {
+    echo "red";
+  }
+}
+
+function changerTexte(){
+  global $etat;
+  if(strpos($etat,'1') !== false) { // toilette occupé
+    echo "Occupé";
+  } else if (strpos($etat,'0') !== false){ // toilette disponible
+    echo "Disponible";
+  } else {
+    echo "Arduino non trouvé. Merci de vérifier les branchements";
+  }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="refresh" content="2"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Toilette connecté</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">Toilette connecté</a>
+    </div>
+  </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h2 class="header center light-blue-text">Est-ce que le toilette est disponible ?</h2>
+      <div class="row center">
+        <h5 class="header col s12 light">Statut du toilette:</h5>
+      </div>
+      <div class="row center">
+        <a href="#" class="btn-large waves-effect waves-light <?php changerCouleur(); ?>" >
+          <?php changerTexte(); ?>
+        </a>
+      </div>
+      <br><br>
+
+    </div>
+  </div>
+
+
+  <div class="container">
+    <div class="section">
+    </div>
+    <br><br>
+
+    <div class="section">
+
+    </div>
+  </div>
+  <footer class="page-footer orange">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">L'équipe</h5>
+          <p class="grey-text text-lighten-4">Richard MONTOUX - Corentin MOREAU - Simon RUIZ - Kevin VIBERT </p>
+
+
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+      EPSI <a class="orange-text text-lighten-3" href="#">Bordeaux</a>
+      </div>
+    </div>
+  </footer>
+
+
+
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="js/materialize.js"></script>
+  <script src="js/init.js"></script>
+
+  </body>
+</html>
